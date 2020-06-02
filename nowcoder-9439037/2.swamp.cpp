85ad2a4499bfe0097af893e3fd52d284a6482589
Carson <25158231+git-hub-tig@users.noreply.github.com>
2020-06-02 13:57:50 +0800

    Update 2.swamp.cpp

--- nowcoder-9439037/2.swamp.cpp
+++ nowcoder-9439037/2.swamp.cpp
@@ -55,3 +55,60 @@ int main()
 	cout << longestWood << endl;
 	return 0;
 }
+///////
+// above is kruskal no unionset
+// below is prim with priority_queue.
+
+import io
+from heapq import heappush, heappop
+import sys
+def process(u, taken, AL, pq):                      # set u as taken and enqueue neighbors of u
+    taken[u] = 1 # set visited
+    for v, w in AL[u]:
+        if (not taken[v]):
+            heappush(pq, (w, v))                    # sort by (inc) weight
+                                                    # then by (inc) id, if same weight or cost.    
+def main():
+    instr="""4 3
+1 2 1
+2 3 1
+3 4 2""" 
+    # mark from 1 not 0. so AL need N+1, also pq process start call
+    # from 1
+    # f=sys.stdin
+    f=io.StringIO(instr)
+    # f = open("kruskal_prim_in.txt", "r")
+    V, E = map(int, f.readline().split())
+    V+=1
+    # Kruskal's algorithm merged with Prim's algorithm
+    # only can not []*3, but can [some]*n 
+    AL = [[] for _ in range(V)]                     # the graph stored in AL
+    EL = []
+    for _ in range(E):
+        # u, v, w = map(int, f.readline().split(" ")) # read as (u, v, w)
+        u, v, w = map(int, f.readline().split()) # read as (u, v, w)
+        EL.append((w, u, v))                        # reorder as (w, u, v)
+        # for sort 
+        AL[u].append((v, w))
+        AL[v].append((u, w))
+    EL.sort()                                       # sort by w, O(E log E)
+    
+    # taken = [0 for i in range(V)]                   # to avoid cycle, no vertex is taken, just visited
+    taken = [0]*V                # to avoid cycle, no vertex is taken
+    pq = []                                         # to select shorter edges
+    process(1, taken, AL, pq)                       # take+process vertex 0
+    mst_cost = 0                                    # no edge has been taken
+    #  also can get as max_edge of mst.
+    num_taken = 0
+    # while len(pq) > 0 and num_taken < V-1:          # until we take V-1 edges
+    while pq and num_taken < V-1:          # until we take V-1 edges, enougth
+        w, u = heappop(pq)
+        if not taken[u]:                            # we have not taken u yet
+            num_taken += 1                          # 1 more edge is taken
+            # mst_cost += w                           # add w of this edge
+            mst_cost=max(mst_cost, w)                           # add w of this edge
+            process(u, taken, AL, pq)               # take+process vertex u
+                                                    # each edge is in pq once
+    print(mst_cost)
+    # print("MST cost = {} (Prim's)".format(mst_cost))
+main()
